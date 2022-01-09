6b0ce478b2d4ff635a224aacd146a525e832a446
Nick Bashor <nbashor@depaul.edu>
2022-01-09 00:02:35 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//const Discord = require('discord.js');
+const {Client, Intents} = require("discord.js");
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+
+
+client.once('ready', () => {
+    console.log('Warli is online.');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//client.login('');
